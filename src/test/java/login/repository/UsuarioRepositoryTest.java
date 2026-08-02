34c3ff9408926e21d797c1a4501ede4407712348
Thiago Gabriel <thiagogabrieel900@gmail.com>
2026-08-02 13:47:58 -0300

    espaço

--- src/test/java/login/repository/UsuarioRepositoryTest.java
+++ src/test/java/login/repository/UsuarioRepositoryTest.java
@@ -3,6 +3,7 @@ package login.repository;
 import login.model.Usuario;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
+
 public class UsuarioRepositoryTest {
 
     private UsuarioRepository repositorio;
