a62afb41e6b49d972a6da11d2df0f860cde35d2e
Thiago Gabriel <thiagogabrieel900@gmail.com>
2026-08-03 19:49:48 -0300

    teste repository em memoria se email está sendo salvo

--- src/test/java/login/repository/UsuarioRepositoryTest.java
+++ src/test/java/login/repository/UsuarioRepositoryTest.java
@@ -3,13 +3,21 @@ package login.repository;
 import login.model.Usuario;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class UsuarioRepositoryTest {
 
-    private UsuarioRepository repositorio;
+    private UsuarioRepository repository;
 
     @BeforeEach
     public void setUp() {
-        repositorio = new UsuarioRepository();
+        repository = new UsuarioRepository();
+    }
+
+   @Test
+    public void deveNaoLancarNullSeEmailForEncontrado(){
+        repository.salvar(new Usuario("teste@gmail.com","123456"));
+        Usuario encontrado = repository.buscarPorEmail("teste@gmail.com");
+        assertNotNull(encontrado);
     }
 }
